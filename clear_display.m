4d3fdf142962b67bf0066463cf7330fb9f4ab1d9
Seyediman Seyedarab <imandevel@gmail.com>
2025-10-15 18:26:56 -0400

    refactor: fix indentation in clear_display and initialize_oled

--- clear_display.m
+++ clear_display.m
@@ -11,11 +11,11 @@ function clear_display(oled)
 %    oled - OLED I2C device object
 %      I2C object
 
-% Set column i2cAddress
-write(oled, [hex2dec('00'), hex2dec('21'), hex2dec('00'), hex2dec('7F')]); 
-% Set page i2cAddress
-write(oled, [hex2dec('00'), hex2dec('22'), hex2dec('00'), hex2dec('07')]); 
-for i = 1:9*8
-    write(oled,[hex2dec('40'),uint64(0)]);
-end
+    % Set column i2cAddress
+    write(oled, [hex2dec('00'), hex2dec('21'), hex2dec('00'), hex2dec('7F')]); 
+    % Set page i2cAddress
+    write(oled, [hex2dec('00'), hex2dec('22'), hex2dec('00'), hex2dec('07')]); 
+    for i = 1:9*8
+        write(oled,[hex2dec('40'),uint64(0)]);
+    end
 end
--- initialize_oled.m
+++ initialize_oled.m
@@ -27,78 +27,78 @@ function [oled,a] = initialize_oled(a, print_ready, varargin)
 %    a - Arduino connection (after ensuring I2C library)
 %      Arduino object
 
-% Checks re-initializes arduino object 
-% if it is not initalized with I2C Library
-if ~ismember('I2C',a.Libraries)
-    port = a.Port;            % given arduino port
-    board = a.Board;          % given arduino board
-    libs = a.Libraries;       % Libraries of given arduino object
-    disp('::: Re-initializing Arduino with I2C Library :::');
-    evalin("base",'clear a')  % clears arduino object in base workspace
-    clear a;                  % clears arduino object in function
-    a = arduino(port,board,'libraries',{libs{1,1}, libs{1,2}, 'I2C'});
-end
-
-% Scans for I2C Devices
-input_check = scanI2CBus(a,0);
-
-% Checks if OLED Screen is attached
-if nargin == 3
-    i2cAddress = varargin{1};
-    if any(strcmp(input_check, i2cAddress))
-        if print_ready == true
-            disp('::: OLED Screen Found :::')
-        end
-    elseif ~any(strcmp(input_check, i2cAddress))
-        disp('::: OLED SCREEN NOT FOUND :::')
-        return
+    % Checks re-initializes arduino object 
+    % if it is not initalized with I2C Library
+    if ~ismember('I2C',a.Libraries)
+        port = a.Port;            % given arduino port
+        board = a.Board;          % given arduino board
+        libs = a.Libraries;       % Libraries of given arduino object
+        disp('::: Re-initializing Arduino with I2C Library :::');
+        evalin("base",'clear a')  % clears arduino object in base workspace
+        clear a;                  % clears arduino object in function
+        a = arduino(port,board,'libraries',{libs{1,1}, libs{1,2}, 'I2C'});
     end
-elseif nargin > 3
-    error("ERROR: Too many inputs");
-else
-    i2cAddress = '0x3C';
-    if any(strcmp(input_check, i2cAddress))
-        if print_ready == true
-            disp('::: OLED Screen Found :::')
+    
+    % Scans for I2C Devices
+    input_check = scanI2CBus(a,0);
+    
+    % Checks if OLED Screen is attached
+    if nargin == 3
+        i2cAddress = varargin{1};
+        if any(strcmp(input_check, i2cAddress))
+            if print_ready == true
+                disp('::: OLED Screen Found :::')
+            end
+        elseif ~any(strcmp(input_check, i2cAddress))
+            disp('::: OLED SCREEN NOT FOUND :::')
+            return
         end
-    elseif ~any(strcmp(input_check, i2cAddress))
-        disp('::: OLED SCREEN NOT FOUND :::')
-        return
+    elseif nargin > 3
+        error("ERROR: Too many inputs");
+    else
+        i2cAddress = '0x3C';
+        if any(strcmp(input_check, i2cAddress))
+            if print_ready == true
+                disp('::: OLED Screen Found :::')
+            end
+        elseif ~any(strcmp(input_check, i2cAddress))
+            disp('::: OLED SCREEN NOT FOUND :::')
+            return
+        end
     end
-end
-
-% OLED Display I2C Object
-oled = device(a,'I2CAddress', i2cAddress);
-
-% Display initialization
-% Turn off the display
-write(oled, [hex2dec('00'), hex2dec('AE')]);
-% Set memory mode
-write(oled, [hex2dec('00'), hex2dec('20'), hex2dec('00')]);
-% Set column address
-write(oled, [hex2dec('00'), hex2dec('21'), hex2dec('00'), hex2dec('7F')]);
-% Set page address
-write(oled, [hex2dec('00'), hex2dec('22'), hex2dec('00'), hex2dec('07')]);
-% Set contrast control
-write(oled, [hex2dec('00'), hex2dec('81'), hex2dec('FF')]);
-write(oled, [hex2dec('00'), hex2dec('C0')]); % Set scan direction
-write(oled, [hex2dec('00'), hex2dec('A0')]); % Set segment remap
-write(oled, [hex2dec('00'), hex2dec('A6')]); % Set normal display
-write(oled, [hex2dec('00'), hex2dec('40')]); % Set display start line
-% Enable charge pump regulator
-write(oled, [hex2dec('00'), hex2dec('8D'), hex2dec('14')]);
-pause(0.1);
-write(oled, [hex2dec('00'), hex2dec('AF')]); % Turn on the display
-
-% Clear garbage data, show startup flair, clear display
-flair = '---------------- MATLAB OLED LIB ARADHYA CHAWLA ----------------';
-clear_display(oled);
-display_write(oled,1,128,1,8,1,flair);
-pause(1);
-clear_display(oled);
-
-% Ready statement
-if print_ready == true
-    disp('::: OLED Display Initialized :::')
-end
+    
+    % OLED Display I2C Object
+    oled = device(a,'I2CAddress', i2cAddress);
+    
+    % Display initialization
+    % Turn off the display
+    write(oled, [hex2dec('00'), hex2dec('AE')]);
+    % Set memory mode
+    write(oled, [hex2dec('00'), hex2dec('20'), hex2dec('00')]);
+    % Set column address
+    write(oled, [hex2dec('00'), hex2dec('21'), hex2dec('00'), hex2dec('7F')]);
+    % Set page address
+    write(oled, [hex2dec('00'), hex2dec('22'), hex2dec('00'), hex2dec('07')]);
+    % Set contrast control
+    write(oled, [hex2dec('00'), hex2dec('81'), hex2dec('FF')]);
+    write(oled, [hex2dec('00'), hex2dec('C0')]); % Set scan direction
+    write(oled, [hex2dec('00'), hex2dec('A0')]); % Set segment remap
+    write(oled, [hex2dec('00'), hex2dec('A6')]); % Set normal display
+    write(oled, [hex2dec('00'), hex2dec('40')]); % Set display start line
+    % Enable charge pump regulator
+    write(oled, [hex2dec('00'), hex2dec('8D'), hex2dec('14')]);
+    pause(0.1);
+    write(oled, [hex2dec('00'), hex2dec('AF')]); % Turn on the display
+    
+    % Clear garbage data, show startup flair, clear display
+    flair = '---------------- MATLAB OLED LIB ARADHYA CHAWLA ----------------';
+    clear_display(oled);
+    display_write(oled,1,128,1,8,1,flair);
+    pause(1);
+    clear_display(oled);
+    
+    % Ready statement
+    if print_ready == true
+        disp('::: OLED Display Initialized :::')
+    end
 end
